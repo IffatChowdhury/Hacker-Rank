197ff613e7da91b60a89adc2720342836ae52a55
Iffat Chowdhury <iffat.chowdhury@utsa.edu>
2016-02-06 01:57:23 -0600

    Update chocolateFeast.cpp

--- chocolateFeast.cpp
+++ chocolateFeast.cpp
@@ -1,3 +1,9 @@
+/*Little Bob loves chocolate, and he goes to a store with $N in his pocket. 
+ * The price of each chocolate is $C. The store offers a discount: for every M 
+ * wrappers he gives to the store, he gets one chocolate for free. How many 
+ * chocolates does Bob get to eat?
+ * */
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -16,10 +22,12 @@ int main() {
         int wrapper = 0;
         // Computer answer
         answer += n / c;
-        wrapper = n / c;
+        wrapper = answer;
         while (wrapper >= m){
-			answer += wrapper / m;
-			wrapper -= wrapper / m;
+			int extraChoc = wrapper / m;
+			wrapper -= extraChoc * m;
+			answer += extraChoc;
+			wrapper += extraChoc;
 			}
         cout<<answer<<endl;
     }
